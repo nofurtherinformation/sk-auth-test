a1adaf749f5ba6af2d8f7e0b9cd9bc52e812bd7a
Dylan <dhalpern@uchicago.edu>
2022-12-19 10:01:51 -0600

    Debug

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,9 @@
 import type { LayoutServerLoad } from "./$types"
 
-export const load: LayoutServerLoad = async (event) => {
+export const load: LayoutServerLoad = async ({ locals }) => {
+  const session = await locals.getSession()
+  console.log(session)
   return {
-    session: await event.locals.getSession(),
+    session
   }
 }
